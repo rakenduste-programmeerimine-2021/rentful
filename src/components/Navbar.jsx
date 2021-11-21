5831532b06688d3c4da3eebe14040588675a371a
Taavi Vestel <55434719+TouringNormal@users.noreply.github.com>
2021-11-21 23:11:02 +0200

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+height: 60px;
+background-color: black;
+`
+
+
+
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <Container>
+            navbar
+            </Container>
+        </div>
+    )
+}
+
+export default Navbar
