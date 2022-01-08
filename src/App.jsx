bc14a1adb1477617f65623dd8bbf4efa74fbe5b4
BuildTools <unconfigured@null.spigotmc.org>
2022-01-08 12:18:35 +0200

    ReactRouterDom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const App = () => {
   //return <Home/>
-  return <ProductList/>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+        <Route path="/product/:id">
+          <ProductList/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
