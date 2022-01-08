a9d53fad4f23a69eca11bbb422a8a7b283703cce
BuildTools <unconfigured@null.spigotmc.org>
2022-01-08 19:04:30 +0200

    Figuring out react-router-dom
    Downgrading from version 6 to 5.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,31 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
-
-import { BrowserRouter as Router, Route, Link, Switch} 
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Link, Routes, Navigate} //Uues versioonis ei ole enam "Switch", vaid "Routes"
         from "react-router-dom";
 
-
+//Route'id on samuti umber tehtud
 const App = () => {
-  //return <Home/>
-  return <Home/>;
+  const user = true;
+  return (
+    <Router>  
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/Cart" element={<Cart/>} />
 
-};
+          <Route path="/Login" render={(name) => <Navigate to="/" />} />
+          <Route path="/Register" element={<Register/>} />
+          <Route path="/Product" element={<Product/>} />
+          <Route path="/Product/:id" element={<Product/>} />
+          <Route path="/ProductList" element={<ProductList/>} />
+          <Route path="/ProductList/:category" element={<ProductList/>}/>
+          <Route path="*" element={<Navigate to="/" />}/> 
+        </Routes>
+    </Router>
+
+  );
+
+}
 
 export default App;
